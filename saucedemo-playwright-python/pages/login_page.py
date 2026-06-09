da77d23120f29348d81c701c3da06a85ea569ca1
An Nguyen <annguyenxax@gmail.com>
2026-06-09 23:08:45 +0700

    Add SauceDemo scaffold and update CI

---
+++ saucedemo-playwright-python/pages/login_page.py
@@ -0,0 +1,15 @@
+class LoginPage:
+    def __init__(self, page):
+        self.page = page
+
+    def open(self):
+        # TODO: Navigate to login page
+        pass
+
+    def login(self, username, password):
+        # TODO: Fill credentials and submit login form
+        pass
+
+    def get_error_message(self):
+        # TODO: Return login error message text
+        pass
